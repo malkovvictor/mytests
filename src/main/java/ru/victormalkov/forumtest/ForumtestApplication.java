39d1b4ad2a70aad09879df5540976dcdcbc8ff71
Victor Malkov <malkov.victor@gmail.com>
2021-03-03 15:29:21 +0300

    initial commit

---
+++ src/main/java/ru/victormalkov/forumtest/ForumtestApplication.java
@@ -0,0 +1,13 @@
+package ru.victormalkov.forumtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/victormalkov/forumtest/ForumtestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.victormalkov.forumtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
