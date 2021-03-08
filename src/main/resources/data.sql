5bb114e7ed1b883b447c4d8c6fb65ee9056820c6
Victor Malkov <malkov.victor@gmail.com>
2021-03-08 13:35:33 +0300

    import test data on bootRun

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into users(name, password, role) values ('qqq www', '\$2y\$12\$6gPmg/G1ye5zfbpGVy1Kku1hqW81B2Gl5puQTeglT5b7qLoAMZUd6', 'default')
+insert into users(name) values ('Rr Tt')
+
+insert into posts(author_id, text) values (1, 'utochka')
+insert into posts(author_id, text) values (1, 'kurochka')
+insert into posts(author_id, text) values (1, 'ejoeggjeojojwroe')
+insert into posts(author_id, text) values (2, 'qrfefefe')
+insert into posts(author_id, text) values (2, 'utochka')
