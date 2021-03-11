0616859bd9cab1ecd3068423dafb3e9365216858
Victor Malkov <malkov.victor@gmail.com>
2021-03-11 19:41:38 +0300

    login tests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into users(name, password, role) values ('qqq www', '$2y$12$6gPmg/G1ye5zfbpGVy1Kku1hqW81B2Gl5puQTeglT5b7qLoAMZUd6', 'ROLE_ADMIN')
-insert into users(name) values ('Rr Tt')
+insert into users(name, password, role) values ('Test User One', '$2y$12$6gPmg/G1ye5zfbpGVy1Kku1hqW81B2Gl5puQTeglT5b7qLoAMZUd6', 'ROLE_ADMIN')
+insert into users(name, password, role) values ('Test User Two', '$2y$12$ASuxdIgRKHDcabkiaZ45puhEYYKUid/ssRElJjejYb8ydExsSdCwG', 'ROLE_USER')
 
 insert into posts(author_id, text) values (1, 'utochka')
 insert into posts(author_id, text) values (1, 'kurochka')
--- src/test/java/ru/victormalkov/forumtest/LoginTests.java
+++ src/test/java/ru/victormalkov/forumtest/LoginTests.java
@@ -1,4 +1,54 @@
 package ru.victormalkov.forumtest;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
 public class LoginTests {
+    @Autowired
+    WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    public void homeShouldRedirectToLoginPage() throws Exception {
+        mvc
+                .perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login.html"));
+    }
+
+    @Test
+    public void randomUrlShouldRedirectToLoginPage() throws Exception {
+        mvc
+                .perform(get("/abracadabra/fjoirjewfoijo")) // chosen by fare dice roll, guaranteed to be random
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login.html"));
+    }
+
+    @Test
+    @WithMockUser("Some User")
+    public void homepageWithAuth() throws Exception {
+        mvc
+                .perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"));
+    }
 }
