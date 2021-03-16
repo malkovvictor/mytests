68a0bb34df57303264b0eac2e5a4396884d43219
Victor Malkov <malkov.victor@gmail.com>
2021-03-17 00:51:30 +0300

    some registration form testing

--- src/test/java/ru/victormalkov/forumtest/RegistrationControllerTest.java
+++ src/test/java/ru/victormalkov/forumtest/RegistrationControllerTest.java
@@ -1,13 +1,10 @@
 package ru.victormalkov.forumtest;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -15,10 +12,12 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.transaction.Transactional;
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 class RegistrationControllerTest {
+    public static final String ERROR_XPATH = "//form//p";
+
     @Autowired
     WebApplicationContext context;
 
@@ -31,51 +30,59 @@ class RegistrationControllerTest {
     }
 
     @Test
+    public void testFormShow() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/registration"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("register"))
+                .andExpect(xpath("//form//input[@id='name']").exists())
+                .andExpect(xpath(ERROR_XPATH).doesNotExist());
+    }
+
+    @Test
     @Transactional
     public void testUserCreation() throws Exception {
-        MvcResult response = mvc.perform(MockMvcRequestBuilders.post("/registration")
+        mvc.perform(MockMvcRequestBuilders.post("/registration")
                 .param("name", "Test user 1")
                 .param("password", "Test password")
                 .param("matchingPassword", "Test password"))
-                .andExpect(MockMvcResultMatchers.status().is3xxRedirection())
-                .andExpect(MockMvcResultMatchers.redirectedUrl("/"))
-                .andReturn();
+                .andExpect(status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/"));
     }
 
     @Test
     @Transactional
     public void userCreationWithoutNameShouldFail() throws Exception {
-        MvcResult response = mvc.perform(MockMvcRequestBuilders.post("/registration")
+        mvc.perform(MockMvcRequestBuilders.post("/registration")
                 .param("Noname", "Test user 1")
                 .param("password", "Test password")
                 .param("matchingPassword", "Test password"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("register"))
-                .andReturn();
+                .andExpect(status().isOk())
+                .andExpect(view().name("register"))
+                .andExpect(xpath(ERROR_XPATH).exists());
     }
 
     @Test
     @Transactional
     public void userCreationWithoutPasswordShouldFail() throws Exception {
-        MvcResult response = mvc.perform(MockMvcRequestBuilders.post("/registration")
+        mvc.perform(MockMvcRequestBuilders.post("/registration")
                 .param("name", "Test user 1")
                 .param("NoPassword", "Test password")
                 .param("matchingPassword", "Test password"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("register"))
-                .andReturn();
+                .andExpect(status().isOk())
+                .andExpect(view().name("register"))
+                .andExpect(xpath(ERROR_XPATH).exists());
     }
 
     @Test
     @Transactional
     public void userCreationWithNotMatchingPasswordShouldFail() throws Exception {
-        MvcResult response = mvc.perform(MockMvcRequestBuilders.post("/registration")
+        mvc.perform(MockMvcRequestBuilders.post("/registration")
                 .param("name", "Test user 1")
                 .param("password", "Test password")
                 .param("matchingPassword", "Not matching password"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.view().name("register"))
-                .andReturn();
+                .andExpect(status().isOk())
+                .andExpect(view().name("register"))
+                .andExpect(xpath(ERROR_XPATH).exists());
     }
 
 }
