3ac39c1b1b5804d4bd085cf1709e21acf7ebd472
Victor Malkov <malkov.victor@gmail.com>
2021-03-17 01:27:18 +0300

    remove unused imports

--- src/main/java/ru/victormalkov/forumtest/HomeController.java
+++ src/main/java/ru/victormalkov/forumtest/HomeController.java
@@ -3,11 +3,7 @@ package ru.victormalkov.forumtest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.context.request.WebRequest;
-import ru.victormalkov.forumtest.dto.UserDTO;
 import ru.victormalkov.forumtest.repository.PostRepository;
 
 @Controller
--- src/main/java/ru/victormalkov/forumtest/services/UserServiceImpl.java
+++ src/main/java/ru/victormalkov/forumtest/services/UserServiceImpl.java
@@ -1,8 +1,6 @@
 package ru.victormalkov.forumtest.services;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
