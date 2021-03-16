96fe9a1eeb55c21ec1ed65e24322bc781a86e6f3
Victor Malkov <malkov.victor@gmail.com>
2021-03-16 21:42:26 +0300

    add annotation for JaCoCo to ignore

--- src/main/java/ru/victormalkov/forumtest/ForumtestApplication.java
+++ src/main/java/ru/victormalkov/forumtest/ForumtestApplication.java
@@ -2,10 +2,12 @@ package ru.victormalkov.forumtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.victormalkov.forumtest.util.ExcludeFromCodeCoverageGenerated;
 
 @SpringBootApplication
 public class ForumtestApplication {
 
+	@ExcludeFromCodeCoverageGenerated
 	public static void main(String[] args) {
 		SpringApplication.run(ForumtestApplication.class, args);
 	}
