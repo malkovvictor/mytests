b07df7063a525d7e0f205614a040899a62daee1d
Victor Malkov <malkov.victor@gmail.com>
2021-03-17 01:24:23 +0300

    add "final"

--- src/main/java/ru/victormalkov/forumtest/security/MyUserDetails.java
+++ src/main/java/ru/victormalkov/forumtest/security/MyUserDetails.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 public class MyUserDetails implements UserDetails {
-    private User user;
+    private final User user;
 
     public MyUserDetails(User user) {
         this.user = user;
