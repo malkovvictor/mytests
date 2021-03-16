5e7224669ed1addf15f71243ed116d93b38a030a
Victor Malkov <malkov.victor@gmail.com>
2021-03-16 13:23:43 +0300

    remove unused imports

--- src/main/java/ru/victormalkov/forumtest/RegistrationController.java
+++ src/main/java/ru/victormalkov/forumtest/RegistrationController.java
@@ -1,23 +1,18 @@
 package ru.victormalkov.forumtest;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
 import ru.victormalkov.forumtest.dto.UserDTO;
 import ru.victormalkov.forumtest.model.User;
 import ru.victormalkov.forumtest.services.UserService;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Slf4j
